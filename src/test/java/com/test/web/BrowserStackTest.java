b97e1776b7116b4db384b075be9ecdf873679a18
Sujay Sawant <sujay.s@browserstack.com>
2022-06-14 17:32:25 +0530

    first commit

---
+++ src/test/java/com/test/web/BrowserStackTest.java
@@ -0,0 +1,71 @@
+package com.test.web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import static org.openqa.selenium.Keys.TAB;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.testng.Assert.assertEquals;
+
+public class BrowserStackTest {
+
+    private WebDriver driver;
+
+    private static final String USERNAME = System.getenv("BROWSERSTACK_USERNAME");
+    private static final String ACCESS_KEY = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    private static final String HUB_URL = "https://hub-cloud.browserstack.com/wd/hub";
+
+    @BeforeMethod(alwaysRun = true)
+    public void setup(Method m) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("project", "BrowserStack Drone CI");
+        caps.setCapability("build", "Drone CI Build");
+        caps.setCapability("name", m.getName() + " - Chrome latest");
+
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "10");
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "latest");
+
+        caps.setCapability("browserstack.user", USERNAME);
+        caps.setCapability("browserstack.key", ACCESS_KEY);
+        caps.setCapability("browserstack.debug", true);
+        caps.setCapability("browserstack.networkLogs", true);
+
+        driver = new RemoteWebDriver(new URL(HUB_URL), caps);
+    }
+
+    @Test
+    public void bStackDemoLogin() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://bstackdemo.com");
+        wait.until(elementToBeClickable(By.id("signin"))).click();
+        wait.until(elementToBeClickable(By.cssSelector("#username input"))).sendKeys("fav_user" + TAB);
+        driver.findElement(By.cssSelector("#password input")).sendKeys("testingisfun99" + TAB);
+        driver.findElement(By.id("login-btn")).click();
+        String username = wait.until(presenceOfElementLocated(By.className("username"))).getText();
+        assertEquals(username, "fav_user", "Incorrect username");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void closeDriver() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\"}}");
+        driver.quit();
+    }
+
+
+}
